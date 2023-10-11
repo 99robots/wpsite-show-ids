4193f3cc066dee1c9f823c3d4a5c87b619abf12f
Fahad Ahmed <40246083+Fahadahmed23@users.noreply.github.com>
2023-10-11 16:24:15 -0700

    PluginUpdated

--- wpsite-show-ids.php
+++ wpsite-show-ids.php
@@ -2,49 +2,92 @@
 /**
  * Plugin Name: Show IDs
  * Plugin URI: https://draftpress.com/products
- * Description: Show IDs on all post, page and taxonomy pages.
- * Version: 1.1.9
+ * Description: Show IDs on all post, page, and taxonomy pages.
+ * Version: 1.1.10
  * Author: DraftPress
  * Author URI: https://draftpress.com/
  * License: GPL2
+ * php version: 7.0
+ * 
+ * @category Plugin
+ * @package  ShowIDs
+ * @author   DraftPress <john.doe@example.com>
+ * @license  GNU General Public License 2 
+ *           (https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+ * @link     https://draftpress.com/products
  */
 
+ 
+/**
+ * Class WPSite_Show_IDs
+ * 
+ * @category Class
+ * @package  ShowIDs
+ * @author   DraftPress <john.doe@example.com>
+ * @license  GNU General Public License 2
+ *          (https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+ * @link     https://draftpress.com/products
+ */
 class WPSite_Show_IDs
 {
 
+    /**
+     * Class constructor. Initializes actions and filters.
+     */
     public function __construct()
     {
 
-        add_action('admin_init', array($this, 'custom_objects'));
-        add_action('admin_head', array($this, 'add_css'));
+        add_action('admin_init', array($this, 'customObjects'));
+        add_action('admin_head', array($this, 'addCss'));
 
         // For Post Management
-        add_filter('manage_posts_columns', array($this, 'add_column'));
-        add_action('manage_posts_custom_column', array($this, 'add_value'), 10, 2);
+        add_filter('manage_posts_columns', array($this, 'addColumn'));
+        add_action('manage_posts_custom_column', array($this, 'addValue'), 10, 2);
 
         // For Page Management
-        add_filter('manage_pages_columns', array($this, 'add_column'));
-        add_action('manage_pages_custom_column', array($this, 'add_value'), 10, 2);
+        add_filter('manage_pages_columns', array($this, 'addColumn'));
+        add_action('manage_pages_custom_column', array($this, 'addValue'), 10, 2);
 
         // For Media Management
-        add_filter('manage_media_columns', array($this, 'add_column'));
-        add_action('manage_media_custom_column', array($this, 'add_value'), 10, 2);
+        add_filter('manage_media_columns', array($this, 'addColumn'));
+        add_action('manage_media_custom_column', array($this, 'addValue'), 10, 2);
 
         // For Link Management
-        add_filter('manage_link-manager_columns', array($this, 'add_column'));
-        add_action('manage_link_custom_column', array($this, 'add_value'), 10, 2);
+        add_filter('manage_link-manager_columns', array($this, 'addColumn'));
+        add_action('manage_link_custom_column', array($this, 'addValue'), 10, 2);
 
         // For Category Management
-        add_action('manage_edit-link-categories_columns', array($this, 'add_column'));
-        add_filter('manage_link_categories_custom_column', array($this, 'add_return_value'), 10, 3);
+        add_action(
+            'manage_edit-link-categories_columns', 
+            array($this, 'addColumn')
+        );
+        add_filter(
+            'manage_link_categories_custom_column', 
+            array($this, 'addReturnValue'), 
+            10, 
+            3
+        );
 
         // For User Management
-        add_action('manage_users_columns', array($this, 'add_column'));
-        add_filter('manage_users_custom_column', array($this, 'add_return_value'), 10, 3);
+        add_action('manage_users_columns', array($this, 'addColumn'));
+        add_filter(
+            'manage_users_custom_column', 
+            array($this, 'addReturnValue'), 
+            10, 
+            3
+        );
 
         // For Comment Management
-        add_action('manage_edit-comments_columns', array($this, 'add_column'));
-        add_action('manage_comments_custom_column', array($this, 'add_value'), 10, 2);
+        add_action(
+            'manage_edit-comments_columns', 
+            array($this, 'add_column')
+        );
+        add_action(
+            'manage_comments_custom_column', 
+            array($this, 'addValue'), 
+            10, 
+            2
+        );
     }
 
     /**
@@ -52,15 +95,23 @@ class WPSite_Show_IDs
      *
      * @return void
      */
-    public function custom_objects()
+    public function customObjects()
     {
 
         // For Custom Taxonomies
         $taxonomies = get_taxonomies(array('public' => true), 'names');
         foreach ($taxonomies as $custom_taxonomy) {
             if (isset($custom_taxonomy)) {
-                add_action('manage_edit-' . $custom_taxonomy . '_columns', array($this, 'add_column'));
-                add_filter('manage_' . $custom_taxonomy . '_custom_column', array($this, 'add_return_value'), 10, 3);
+                add_action(
+                    'manage_edit-' . $custom_taxonomy . '_columns', 
+                    array($this, 'addColumn')
+                );
+                add_filter(
+                    'manage_' . $custom_taxonomy . '_custom_column', 
+                    array($this, 'addReturnValue'), 
+                    10, 
+                    3
+                );
             }
         }
 
@@ -68,8 +119,16 @@ class WPSite_Show_IDs
         $post_types = get_post_types(array('public' => true), 'names');
         foreach ($post_types as $post_type) {
             if (isset($post_type)) {
-                add_action('manage_edit-' . $post_type . '_columns', array($this, 'add_column'));
-                add_filter('manage_' . $post_type . '_custom_column', array($this, 'add_return_value'), 10, 3);
+                add_action(
+                    'manage_edit-' . $post_type . '_columns', 
+                    array($this, 'addColumn')
+                );
+                add_filter(
+                    'manage_' . $post_type . '_custom_column', 
+                    array($this, 'addReturnValue'), 
+                    10, 
+                    3
+                );
             }
         }
     }
@@ -79,66 +138,71 @@ class WPSite_Show_IDs
      *
      * @return void
      */
-    public function add_css()
+    public function addCss()
     {
         ?>
-		<style type="text/css">
-			#wpsite-show-ids {
-				width: 50px;
-			}
-		</style>
-		<?php
-}
+        <style type="text/css">
+            #wpsite-show-ids {
+                width: 50px;
+            }
+        </style>
+        <?php
+    }
 
     /**
-     * Adds column to edit screen
+     * Adds a column to the edit screen.
      *
-     * @param mixed $cols
+     * @param mixed $cols The existing columns.
+     * 
      * @return void
      */
-    public function add_column($cols)
+    public function addColumn($cols)
     {
-
         $cols['wpsite-show-ids'] = 'ID';
-
         return $cols;
     }
 
     /**
-     * Adds id value
+     * Adds id value to a column.
      *
-     * @param mixed $column_name
-     * @param mixed $id
+     * @param mixed $column_name 
+     * @param mixed $id          
+     * 
      * @return void
      */
-    public function add_value($column_name, $id)
+    public function addValue($column_name, $id)
     {
         if ('wpsite-show-ids' === $column_name) {
             echo $id;
         }
     }
 
+
     /**
-     * Adds id value
+     * Adds an ID value as a return value for a column.
      *
-     * @param mixed $value
-     * @param mixed $column_name
-     * @param mixed $id
+     * @param mixed $value       The current column value.
+     * @param mixed $column_name The name of the column.
+     * @param mixed $id          The ID value.
+     *
      * @return void
      */
-    public function add_return_value($value, $column_name, $id)
+    public function addReturnValue($value, $column_name, $id)
     {
-
         if ('wpsite-show-ids' === $column_name) {
             $value = $id;
         }
 
         return $value;
     }
+
 }
 
 /**
  * Start
+ * 
  * @var WPSite_Show_IDs
  */
 new WPSite_Show_IDs;
+
+?>
